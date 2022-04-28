5a7007050f3076aa49821906d50e9cbc83afae6d
kingfeekra <fiachraking@gmail.com>
2022-04-28 17:36:55 +0100

    Grid is now 16x16
    Rework loops so that none are copied and pasted
    
    Change height and width of boxes in grid

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 const container = document.querySelector("#container");
+const divRows = [];
 
+//creates 16 divs intended to act as rows, appends to container
+for (let i = 0; i < 16; i++) { 
+    divRows[i] = document.createElement("div");
+    divRows[i].classList.add("divRow");
+    container.appendChild(divRows[i]);
+}
 
-for (let i = 0; i < 4; i++) {
-    const divRow = document.createElement("div");
-    divRow.classList.add("divRow");
-    container.appendChild(divRow);
-}
-for (let i = 0; i < 4; i++) {
+//creates 16 boxes for each row, appends to each row
+for (let i = 0; i < 16; i++) {
     const divRowList = document.querySelectorAll(".divRow");
     const divBox = document.createElement("div");
     divBox.classList.add("divBox");
-    divRowList[0].appendChild(divBox);
-}
-for (let i = 0; i < 4; i++) {
-    const divRowList = document.querySelectorAll(".divRow");
-    const divBox = document.createElement("div");
-    divBox.classList.add("divBox");
-    divRowList[1].appendChild(divBox);
-}
-for (let i = 0; i < 4; i++) {
-    const divRowList = document.querySelectorAll(".divRow");
-    const divBox = document.createElement("div");
-    divBox.classList.add("divBox");
-    divRowList[2].appendChild(divBox);
-}
-for (let i = 0; i < 4; i++) {
-    const divRowList = document.querySelectorAll(".divRow");
-    const divBox = document.createElement("div");
-    divBox.classList.add("divBox");
-    divRowList[3].appendChild(divBox);
-}
+    for (j = 0; j < divRowList.length; j++) {
+    divRowList[j].appendChild(divBox.cloneNode(true)); //cloneNode is needed to ensure 16 divs are appended to every row
+    }
+}// grid has now been created
+
 
