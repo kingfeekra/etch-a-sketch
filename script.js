eebf774030100fb25054599aa914bb517f2cdf91
kingfeekra <fiachraking@gmail.com>
2022-04-29 18:39:28 +0100

    Update change square button functionality
    If user enters a number greater than 100, or a string/nan
    grid does not reset or change.
    
    Add heading to index.html
    
    Add comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container");
 const divRows = [];
+
 let numberOfBoxes = 16;
 let boxHeight = 640/16;
 let boxWidth = 640/16;
@@ -27,18 +28,18 @@ for (let i = 0; i < numberOfBoxes; i++) {
 //sketch functionality, box color changes to random when mouse hovers over
 const divBoxList = document.querySelectorAll(".divBox"); 
 for (let i = 0; i < divBoxList.length; i++) {
-divBoxList[i].addEventListener("mouseover", () => {
+    divBoxList[i].addEventListener("mouseover", () => {
     divBoxList[i].style.cssText = `background-color: rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()}); 
     height: ${boxHeight}px; width: ${boxWidth}px; border: 1px solid black;`;
     
 });
 }
 }
-grid();
+grid(); // run grid creation function
 
 function randomNumber() {
-    console.log(Math.floor(Math.random()*255));
-    return Math.floor(Math.random()*255);
+    console.log(Math.floor(Math.random()*256)); //random number between 0 and 255
+    return Math.floor(Math.random()*256);
 }
 
 function gridPrompt() {
@@ -46,20 +47,32 @@ function gridPrompt() {
     };
 
 const btn = document.querySelector("#resetButton");
-btn.addEventListener("click", () => {
+
+btn.addEventListener("click", () => { //when button is clicked, prompt user to change number of squares, grid then resets
     resetGrid(gridPrompt())
 });
 
-let resetGrid = function(num) {
+let resetGrid = function(num) { //grid reset function
+    if (num === null){ //if user cancels or doesn't enter a number, don't reset
+        return;
+    }
+    else if (parseInt(num) > 100) {
+        alert("Cannot be higher than 100.");
+        return;
+    }
+
     const removeDivs = document.querySelectorAll(".divRow");
+
     for (let i = 0; i < removeDivs.length; i ++) {
     container.removeChild(removeDivs[i]);
     }
-    if (parseInt(num) <= 100) {
+
+    if (parseInt(num) <= 100) { //old squares are removed, new squares are created
         numberOfBoxes = parseInt(num);
         boxHeight = 640/numberOfBoxes;
         boxWidth = 640/numberOfBoxes;
         console.log(numberOfBoxes);
         grid();
     }
+    
 }
